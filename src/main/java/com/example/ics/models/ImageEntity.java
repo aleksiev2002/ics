378aba60d262afcc2864b79f565f390f0393388c
Needlessbug <100784917+aleksiev2002@users.noreply.github.com>
2023-04-28 16:29:20 +0300

    Adding Image entity.

---
+++ src/main/java/com/example/ics/models/ImageEntity.java
@@ -0,0 +1,11 @@
+package com.example.ics.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "images")
+public class ImageEntity {
+    public ImageEntity() {
+    }
+}
